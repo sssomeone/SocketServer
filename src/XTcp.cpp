2163463e981516e72d8b50c9f65c36fdc7e53e17
sssomeone <pengfei1998@foxmail.com>
2021-09-11 19:22:10 +0800

    init

--- src/XTcp.cpp
+++ src/XTcp.cpp
@@ -1,8 +1,24 @@
+/*********************************************************************************************
+**                                                                                          **
+**                                 Jiedi(China nanjing)Ltd.                                 **
+**	                          版权归夏曹俊所有，此代码作为学习参考                          **                                                                            **
+*********************************************************************************************/
+
+/*****************************    联系方式   *************************************************
+** 购买课程的同学可以加群398872593咨询讨论网络编程技术问题，老师一有时间就会解答您的问题
+** 加群请注明购买平台和购买账号，一般一天内就会审核通过
+** 如果觉得课程内容对您有帮助，希望给出五星好评，并留下您的意见，我会根据您的意见调整修改课程
+** Contact       : xiacaojun@qq.com
+**        博客   : http://blog.csdn.net/jiedichina
+**        QQ	 : 99981441
+**		  QQ群	 ：398872593
+**   微信公众号  : jiedi2007
+**
+********************************************************************************************/
+#include "XTcp.h"
 #include <string.h>
 #include <stdio.h>
-#include <string>
 #include <stdlib.h>
-
 #ifdef WIN32
 #include <windows.h>
 #define socklen_t int
@@ -16,24 +32,6 @@
 
 #include <thread>
 using namespace std;
-
-class XTcp
-{
-public:
-	int CreateSocket();
-	bool Bind(unsigned short port);
-	XTcp Accept();
-	void Close();
-	int Recv(char* buf, int bufsize);
-	int Send(const char* buf, int sendsize);
-	XTcp();
-	virtual ~XTcp();
-	int sock = 0;
-	unsigned short port = 0;
-	std::string ip;
-
-};
-
 
 XTcp::XTcp()
 {
@@ -73,11 +71,11 @@ bool XTcp::Bind(unsigned short port)
 	listen(sock, 10);
 	return true;
 }
-int XTcp::Recv(char* buf, int bufsize)
+int XTcp::Recv(char *buf, int bufsize)
 {
 	return recv(sock, buf, bufsize, 0);
 }
-int XTcp::Send(const char* buf, int size)
+int XTcp::Send(const char *buf, int size)
 {
 	int s = 0;
 	while (s != size)
--- src/main.cpp
+++ src/main.cpp
@@ -1,104 +1,164 @@
 #include "XTcp.cpp"
 
 #include<string>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 #include<regex>
 using namespace std;
 
-class HttpThread {
+class HttpThread{
 public:
-	void Main() {
-		char buf[10000] = { 0 };
+	void Main(){
+		char buf[10000]={0};
 
-		int recvlen = client.Recv(buf, sizeof(buf) - 1);
-		if (recvlen <= 0)
+		
+		int recvlen=client.Recv(buf,sizeof(buf)-1);
+		if(recvlen<=0)
 		{
 			return Close();
 		}
-		printf("-------------recv----------------------\n%s---------------recv----------------\n\n", buf);
+		printf("-------------recv----------------------\n%s---------------recv----------------\n\n",buf);
 
-		string src = buf;
+		string src=buf;
 
-		string pattern = "^([A-Z]+) /([a-zA-Z0-9]*([.][a-zA-Z0-9]*)?)[?]?(.*) HTTP/1";
+
+		/*
+		   ?????????????????????????????????????????????src: GET /?nihaoa&aaaaa HTTP/1.1
+		    Host: 8.210.70.64:34567
+			Upgrade-Insecure-Requests: 1
+			User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36
+			Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,;q=0.8,application/signed-exchange;v=b3;q=0.9
+			Accept-Encoding: gzip, deflate
+			Accept-Language: zh-CN,zh;q=0.9
+			Connection: close
+			type: [GET]
+			path: [/]
+			filetype: []
+			query: [nihaoa&aaaaa]
+			sended size = 90
+	    */
+
+
+
+		
+			// / index.html /ff
+			//string pattern="^([A-Z]+) /([a-zA-Z0-9]*([.].*)?) HTTP/1";
+		string pattern="^([A-Z]+) /([a-zA-Z0-9]*([.][a-zA-Z0-9]*)?)[?]?(.*) HTTP/1";
 		regex r(pattern);
 		smatch mas;
-		regex_search(src, mas, r);
-		if (mas.size() == 0) {
-			printf("%s failed\n", pattern.c_str());
+		regex_search(src,mas,r);
+		if(mas.size()==0){
+			printf("%s failed\n",pattern.c_str());
 			return Close();
 		}
 
-		string type = mas[1];
-		string path = "/";
-		path += mas[2];
+		string type=mas[1];
+		string path="/";
+		path+=mas[2];
 
 		string filetype = mas[3];
 
-		string query = mas[4];
+		string query=mas[4];
 
 
-		if (filetype.size() > 0)
-			filetype = filetype.substr(1, filetype.size() - 1);
+		if(filetype.size()>0)
+			filetype=filetype.substr(1,filetype.size()-1);
 
-		printf("type: [%s]\npath: [%s]\nfiletype: [%s]\nquery: [%s]\n", \
-			type.c_str(), path.c_str(), filetype.c_str(), query.c_str());
+		printf("type: [%s]\npath: [%s]\nfiletype: [%s]\nquery: [%s]\n",\
+			type.c_str(),path.c_str(),filetype.c_str(),query.c_str());
 
-		if (type != "GET") {
+
+
+		//download twitter   query
+		if(query.size()>=8&&query.substr(0,8)=="twitter&"){
+
+			//獲取輸入的twitter下載的url
+			string url=query.substr(8);
+
+			printf("start download  %s\n",query.c_str());
+
+			int id=fork();
+			if(id==0){
+				//execl("proxychains","python3","twitter-dl.py",query.c_str());
+				//execl("/usr/bin/proxychains","proxychains","python3","twitter-dl.py","-w","500",query.c_str(),NULL);
+
+				int id2=fork();
+
+				if(id2==0){
+					if(execl("/usr/local/bin/you-get","you-get",url.c_str(),"-o", "twitter",NULL)==-1)
+						{printf("download %s failed\n",url.c_str());}
+				}
+				else {
+					int status;
+					waitpid(id2, &status, 0);
+					printf("download %s success\n",url.c_str());
+
+					int id3=fork();
+					int status2;
+					if(id3==0){
+						execl("/usr/bin/python3","python3","/home/wpengfei/downTwi/py/aliyunpan/main.py","upload","-p","/home/wpengfei/downTwi/twitter",NULL);
+					}
+					else{
+						waitpid(id3, &status2, 0);
+						execl("/bin/rm","rm","-rf","/home/wpengfei/downTwi/twitter",NULL);
+					}
+					//python3 home/wpengfei/downTwi/py/aliyunpan/main.py upload -p  
+				}
+				
+			}
+
+
+		}
+
+
+
+
+		if(type != "GET"){
 			printf("not GET\n");
 			return Close();
 		}
-		string filename = path;
-		if (path == "/") {
-			filename = "/index.html";
+		string filename=path;
+		if(path=="/"){
+			filename="/index.html";
 		}
 
-		string filepath = "www";
-		filepath += filename;
+		string filepath="www";
+		filepath+=filename;
 
-		string cmd;
-		if (filetype == "php") {
-			cmd = "proxychains python3 twitter-dl.py";
-			cmd += (" " + query);
-			printf("downloading  %s\n", query.c_str());
-			printf("exec %s\n", cmd.c_str());
-		}
+
 
 
 
 		//回应http GET请求
-		string rmsg = "HTTP/1.1 200 OK\r\n";
-		rmsg += "Server: XHttp\r\n";
-		rmsg += "Content-Type: text/html\r\n";
+		string rmsg="HTTP/1.1 200 OK\r\n";
+		rmsg+="Server: XHttp\r\n";
+		rmsg+="Content-Type: text/html\r\n";
 		//rmsg+="Content-Type: image/gif\r\n";
-		rmsg += "Content-Length: ";
+		rmsg+="Content-Length: ";
 
-		//char bsize[128]={0};
-		//sprintf(bsize,"%d",filesize);
+			//char bsize[128]={0};
+			//sprintf(bsize,"%d",filesize);
 
 
-	//rmsg+="10\r\n";
-		rmsg += "11";
-		rmsg += "\r\n\r\n";
-		rmsg += "received ok";
+		//rmsg+="10\r\n";
+		rmsg+="11";
+		rmsg+="\r\n\r\n";
+		rmsg+="received ok";
 
 		//发送消息头
-		int sendedsize = client.Send(rmsg.c_str(), rmsg.size());
+		int sendedsize=client.Send(rmsg.c_str(),rmsg.size());
 		printf("sended size = %d\n", sendedsize);
 
 
-
-		buf[recvlen] = '\0';
+		
+		buf[recvlen]='\0';
 		printf("------------------send----------------%s\n----------------send---------------\n\n", rmsg.c_str());
 		Close();
-		//int id = fork();
-		//if (id == 0) {
-		//	//execl("proxychains","python3","twitter-dl.py",query.c_str());
-		//	//execl("/usr/bin/proxychains","proxychains","python3","twitter-dl.py","-w","500",query.c_str(),NULL);
-		//	if (execl("/usr/bin/proxychains", "proxychains", "you-get", query.c_str(), NULL) == -1)
-		//		printf("wrong===============================\n");
-		//}
+		
 		//system(cmd.c_str());
 	}
-	void Close() {
+	void Close(){
 		client.Close();
 		delete this;
 	}
@@ -107,24 +167,24 @@ public:
 
 int main(int argc, char* argv[]) {
 
-	unsigned short port = 8080;
-	if (argc > 1) {
-		port = atoi(argv[1]);
+	unsigned short port=8080;
+	if(argc>1){
+		port=atoi(argv[1]);
 	}
 	XTcp server;
 	server.CreateSocket();
 	server.Bind(port);
 
-	while (true) {
+	while(true){
 		XTcp client = server.Accept();
-		HttpThread* th = new HttpThread();
-		th->client = client;
+		HttpThread *th=new HttpThread();
+		th->client=client;
 
 		//???
-		thread sth(&HttpThread::Main, th);
+		thread sth(&HttpThread::Main,th);
 		sth.detach();
 	}
-
+	
 	server.Close();
 	return 0;
 }
